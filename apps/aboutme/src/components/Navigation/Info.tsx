6030ea511f2158bd8172a5c09856e14787a1ffea
hwanheejung <hwanheejung117@gmail.com>
2024-09-09 17:29:56 +0900

    #5 feat: added usermeta

--- apps/aboutme/src/components/Navigation/Info.tsx
+++ apps/aboutme/src/components/Navigation/Info.tsx
@@ -1,5 +1,20 @@
+import { USERMETA } from "contents/meta";
+import LocationIcon from "public/icons/location.svg";
+import OrganizationIcon from "public/icons/organization.svg";
+
 const Info = () => {
-  return <div></div>;
+  return (
+    <div className="mt-16 flex flex-col gap-2 pl-2 text-sm opacity-70">
+      <div className="flex items-center gap-2">
+        <LocationIcon />
+        <span>{USERMETA.location}</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <OrganizationIcon />
+        <span>{USERMETA.school}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
--- apps/aboutme/src/components/Navigation/index.tsx
+++ apps/aboutme/src/components/Navigation/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Menu from "./Menu";
 import Info from "./Info";
 import Links from "./Links";
+import { USERMETA } from "contents/meta";
 
 const MENUS = [
   {
@@ -22,14 +23,14 @@ const Navigation = () => {
   return (
     <nav className="px-5 py-7">
       <h1 className="px-2 pr-4 text-2xl font-bold text-main">
-        <Link href="/">Hwanhee Jeong</Link>
+        <Link href="/">{USERMETA.name}</Link>
       </h1>
-      <Info />
       <ul className="mt-10 flex flex-col gap-2">
         {MENUS.map((menu) => (
           <Menu key={menu.link} title={menu.title} link={menu.link} />
         ))}
       </ul>
+      <Info />
       <Links />
     </nav>
   );
