9bf8f794a7a4f6ea490c60d32c86e7f110da59ef
hwanheejung <hwanheejung117@gmail.com>
2024-09-09 14:57:04 +0900

    #5 chore: husky setup

--- apps/aboutme/mdx-components.tsx
+++ apps/aboutme/mdx-components.tsx
@@ -3,21 +3,18 @@ import Image, { ImageProps } from "next/image";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // h1: ({ children }) => (
-    //   <h1 style={{ fontSize: "2.5rem" }} className="font-bold">
-    //     {children}
-    //   </h1>
-    // ),
-    // h2: ({ children }) => (
-    //   <h2 style={{ fontSize: "2rem" }} className="font-bold">
-    //     {children}
-    //   </h2>
-    // ),
-    // h3: ({ children }) => (
-    //   <h3 style={{ fontSize: "1.5rem" }} className="font-bold mt-8 mb-3">
-    //     {children}
-    //   </h3>
-    // ),
+    blockquote: ({ children }) => (
+      <blockquote
+        style={{
+          color: "var(--color-primary)",
+          opacity: 0.8,
+          fontWeight: 200,
+          quotes: "none",
+        }}
+      >
+        {children}
+      </blockquote>
+    ),
     img: (props) => (
       <Image
         sizes="100vw"
